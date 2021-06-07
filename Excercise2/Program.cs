cfc985465902b3854c554206840555f95cf1577b
Arjay Orbita <planetorbita@gmail.com>
2021-06-07 11:22:30 -0700

    Added Excercise2

---
+++ Excercise2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using HeadFirstCSharp8_1;
+
+namespace Excercise2
+{
+    class Program
+    {
+        public static Random RNG = new Random();
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static Card RandomCard()
+        {
+            return new Card((Values)RNG.Next(1, 14), (Suits)RNG.Next(4));
+        }
+
+    }
+}
